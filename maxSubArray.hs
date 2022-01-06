f7edcd6d53342b46cb8fd4d2cab91da5c2bfa6c6
IsakJones <isak.jones.98@gmail.com>
2022-01-06 23:20:38 +0100

    updated msa

--- maxSubArray.hs
+++ maxSubArray.hs
@@ -1,10 +1,13 @@
+-- Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
--- Fold over list
+-- A subarray is a contiguous part of an array.
+
+-- https://leetcode.com/problems/maximum-subarray/
 
 maxSubArray :: [Int] -> Int
 maxSubArray = maximum . scanl1 sumIfPositive 
 
-sumIfPositive :: Int -> (Int -> Int)
-sumIfPositive x
-    | x > 0 = (x+)
-    | otherwise = id
+sumIfPositive :: Int -> Int -> Int
+sumIfPositive x y
+    | x > 0 = x + y
+    | otherwise = y
